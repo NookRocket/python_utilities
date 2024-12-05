2afe660d02b24d8412afd8c1e3160fb53451a948
NOOKJP <58026244+NookRocket@users.noreply.github.com>
2024-12-05 17:33:18 +0700

    Update reload_modules.py

--- reload_modules.py
+++ reload_modules.py
@@ -1,14 +1,17 @@
 import sys
 import importlib
 modules = sys.modules.copy()
-removed = {}
+removed_mod = {}
+# Specify the prefix to avoid reload negligible modules
+prefix_module = '' # e.g 'snow.'
+
 for key, module in modules.items():
-    if key.startswith('snow.') and module:
+    if key.startswith(prefix_module) and module:
         if module:
-            removed[key] = module
+            removed_mod[key] = module
             del sys.modules[key]
 
-for key, module in removed.items(): 
+for key, module in removed_mod.items(): 
     try:
         importlib.import_module(key, module)
     except Exception as err:
