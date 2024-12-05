810e32654f6d85b4f519d9c8a9de910dd55a9511
NOOKJP <58026244+NookRocket@users.noreply.github.com>
2024-12-05 17:56:05 +0700

    Update reload_modules.py

--- reload_modules.py
+++ reload_modules.py
@@ -3,13 +3,12 @@ import importlib
 modules = sys.modules.copy()
 removed_mod = {}
 # Specify the prefix to avoid reload negligible modules
-prefix_module = '' # e.g 'snow.'
+prefix_module = 'snow.'
 
 for key, module in modules.items():
-    if key.startswith(prefix_module) and module:
-        if module:
-            removed_mod[key] = module
-            del sys.modules[key]
+    if module and key.startswith(prefix_module):
+        removed_mod[key] = module
+        del sys.modules[key]
 
 for key, module in removed_mod.items(): 
     try:
